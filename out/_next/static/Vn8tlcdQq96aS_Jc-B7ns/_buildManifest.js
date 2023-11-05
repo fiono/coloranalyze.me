7c1f230a4b85e74d79849d86581d349858578462
fiono <frcondon@gmail.com>
2023-11-04 22:18:02 -0400

    rebuild

--- out/_next/static/Vn8tlcdQq96aS_Jc-B7ns/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-5009c039add1b9c1.js"],"/_error":["static/chunks/pages/_error-b646007f40c4f0a8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
