02ef90923bf41b84862b1ad8802e4976544283f4
fiono <frcondon@gmail.com>
2023-11-10 21:38:27 -0500

    Add image uploader

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,25 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import ImageUpload from "./ImageUpload";
 
 import "@fontsource/young-serif";
 
 function App() {
+  const palette = {
+    primary: {
+      main: "#E3D026",
+      light: "#E9DB5D",
+      dark: "#A29415",
+      contrastText: "#242105",
+    },
+  };
+
   const theme = createTheme({
     typography: {
       fontFamily: ['"Young Serif"', "serif"].join(","),
     },
+    palette: palette,
   });
 
   return (
@@ -19,8 +30,11 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
 
-        <Container>
-          <Typography variant="h2">🌈 Seasonal Color Analysis</Typography>
+        <Container maxWidth="lg">
+          <Typography variant="h2" align="center">
+            🌈 Seasonal Color Analysis
+          </Typography>
+          <ImageUpload palette={palette} />
         </Container>
       </ThemeProvider>
     </React.Fragment>
---
+++ src/ImageUpload.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+
+import ImageUploading from "react-images-uploading";
+
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
+function ImagePreview({ imageLocation }) {
+  return <img src={imageLocation} alt="" height="250" />;
+}
+
+export default function ImageUpload({ palette }) {
+  const [images, setImages] = React.useState([]);
+
+  const onChange = (imageList) => {
+    console.log(imageList);
+    setImages(imageList);
+  };
+
+  const image =
+    images.length > 0 ? images[0]["data_url"] : "/image_placeholder.png";
+  const imageElement = <ImagePreview imageLocation={image} />;
+
+  return (
+    <ImageUploading
+      value={images}
+      onChange={onChange}
+      maxNumber={1}
+      dataURLKey="data_url"
+    >
+      {({ isDragging, dragProps, onImageUpload }) => (
+        <Container {...dragProps}>
+          <Box
+            textAlign="center"
+            sx={{
+              m: 10,
+              p: 5,
+              w: 100,
+              h: 40,
+              color: palette.primary.main,
+              border: 2,
+              borderRadius: 4,
+            }}
+          >
+            <Button variant="contained" onClick={onImageUpload}>
+              {isDragging ? "Drag Image" : "Upload Image"}
+            </Button>
+            <Box sx={{ m: 2 }}>{imageElement}</Box>
+          </Box>
+        </Container>
+      )}
+    </ImageUploading>
+  );
+}
